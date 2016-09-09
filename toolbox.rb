ef334c354fab871371eb6307abdc046ed5b278b3
Tanner Nelson <me@tanner.xyz>
2016-09-08 21:43:57 -0400

    toolbox

--- toolbox.rb
+++ toolbox.rb
@@ -4,10 +4,10 @@ class Toolbox < Formula
   sha1 "0a323c749241b0dcb313b3d4cc9411d8b34bbf75"
 
   def install
-  	git clone https://github.com/vapor/toolbox.git
-  	cd toolbox
-  	swift build -c release
-  	mv .build/release/Executable vapor
+  	system "git", "clone", "https://github.com/vapor/toolbox.git"
+  	system "cd", "toolbox"
+  	system "swift", "build", "-c", "release"
+  	system "mv", ".build/release/Executable", "vapor"
   	bin.install "vapor"
   end
 end
