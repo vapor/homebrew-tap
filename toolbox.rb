7901a75b7de9f01664427304c4bdf457677b5d01
Tanner Nelson <me@tanner.xyz>
2016-09-08 21:46:57 -0400

    toolbox

--- toolbox.rb
+++ toolbox.rb
@@ -1,13 +1,15 @@
+require "formula"
+
 class Toolbox < Formula
   homepage "http://vapor.codes"
   url "https://github.com/vapor/toolbox"
   sha1 "0a323c749241b0dcb313b3d4cc9411d8b34bbf75"
 
   def install
-  	system "git", "clone", "https://github.com/vapor/toolbox.git"
-  	system "cd", "toolbox"
-  	system "swift", "build", "-c", "release"
-  	system "mv", ".build/release/Executable", "vapor"
-  	bin.install "vapor"
+    system "git", "clone", "https://github.com/vapor/toolbox.git"
+    system "cd", "toolbox"
+    system "swift", "build", "-c", "release"
+    system "mv", ".build/release/Executable", "vapor"
+    bin.install "vapor"
   end
 end
