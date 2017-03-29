63f471395b5d41c40d40d62e1fde49c67ba5c1e4
Tanner Nelson <me@tanner.xyz>
2017-03-29 13:24:42 +0200

    pre-install vapor

--- vapor-toolbox.rb
+++ vapor-toolbox.rb
@@ -1,18 +1,13 @@
-$version = "1.0.8"
-$hash = "c8f7da9a02d73ddc28c2667a093d6281950eb9c6f3903e1693d048e04c150173"
-
 class VaporToolbox < Formula
   homepage "https://vapor.codes"
-  url "https://github.com/vapor/toolbox/archive/#{$version}.tar.gz"
-  sha256 $hash
-  version $version
+  version "1.0.8"
+  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-10.12"
+  sha256 "00112c01237373f26372cc02722c0a04c43292906807642c8873925953c9e9a2"
 
   depends_on "ctls" => :run
 
   def install
-    system "sed -i '.bak' 's/master/#{$version}/g' Sources/Executable/main.swift"
-    system "unset CC; swift build -c release"
-    system "mv .build/release/Executable vapor"
+    system "mv macOS-10.12 vapor"
     bin.install "vapor"
   end
 end
