e583e4cda860073563a7ec86ae4fe8cd499f8fcb
Tanner Nelson <me@tanner.xyz>
2017-03-29 13:41:07 +0200

    update vapor toolbox

--- vapor.rb
+++ vapor.rb
@@ -2,7 +2,7 @@ class Vapor < Formula
   homepage "https://vapor.codes"
   version "1.0.8"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-10.12"
-  sha256 "00112c01237373f26372cc02722c0a04c43292906807642c8873925953c9e9a2"
+  sha256 "7abe9fe1585a908d4c369c450faa62b4bd9234f97e31003cb0690bc6cad9401e"
 
   depends_on "ctls" => :run
 
