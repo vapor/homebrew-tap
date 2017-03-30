3b3460d47d97286d06d4ada4dccb09c126353f1e
Logan Wright <logan.william.wright@gmail.com>
2017-03-30 15:30:15 +0200

    fix sha

--- vapor-beta.rb
+++ vapor-beta.rb
@@ -2,7 +2,7 @@ class VaporBeta < Formula
   homepage "https://vapor.codes"
   version "2.0.0-beta.1"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra"
-  sha256 "9758634f30ebccfb9b5ce724bfc1471ae97e2f845e4a8ea5de40c4687e8841fe"
+  sha256 "39fc51449ba3d5c584582d11ca2ac672153b2018e82d81421c733e002710291d"
 
   depends_on "ctls" => :run
 
