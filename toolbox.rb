74f5adbf4733a05b0b5959ba001663063706955d
Logan Wright <logan.william.wright@gmail.com>
2017-04-06 15:22:39 +0200

    1.0.9

--- toolbox.rb
+++ toolbox.rb
@@ -1,8 +1,8 @@
 class Toolbox < Formula
   homepage "https://vapor.codes"
-  version "1.0.8"
+  version "1.0.9"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra"
-  sha256 "cc6e52823328b900784f43fd1af7b7cd8c60f3434d2ebf21a21039e7c1d81d1e"
+  sha256 "f801c1b0a563980766b3924f58d4cd15341a478f6dab7e7bbbcc898bcf4b49be"
 
   depends_on "ctls" => :run
 
