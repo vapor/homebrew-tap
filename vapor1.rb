34371530e2af5b2c29863db16cfc5044974e2ee5
tanner0101 <me@tanner.xyz>
2017-05-17 16:59:29 +0100

    vapor 1

---
+++ vapor1.rb
@@ -0,0 +1,13 @@
+class Vapor1 < Formula
+  homepage "https://vapor.codes"
+  version "1.0.9"
+  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra"
+  sha256 "f801c1b0a563980766b3924f58d4cd15341a478f6dab7e7bbbcc898bcf4b49be"
+
+  depends_on "ctls" => :run
+
+  def install
+    system "mv macOS-sierra vapor1"
+    bin.install "vapor1"
+  end
+end
