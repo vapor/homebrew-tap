676f28df17dfab86b986bf8bb2a0ccc01ec8e843
tanner0101 <me@tanner.xyz>
2017-05-19 15:01:11 +0100

    vapor toolbox 2.0.3

--- vapor.rb
+++ vapor.rb
@@ -1,8 +1,8 @@
 class Vapor < Formula
   homepage "https://vapor.codes"
-  version "2.0.2"
+  version "2.0.3"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra"
-  sha256 "ef8f6a8c693a451553740260032b055c3c552aab6190686b16e765e8b5ea7799"
+  sha256 "a1c60282db72492aa4d9f705adb78ed2f585736305af3fc3fccf51f3e063c05f"
 
   depends_on "ctls" => :run
 
