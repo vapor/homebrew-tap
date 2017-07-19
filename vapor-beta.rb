ac33d09c44788b6d1567a1771b9e6c7eea2af13a
Tanner Nelson <me@tanner.xyz>
2017-07-19 12:14:49 -0400

    upgrade toolbox to beta 12

--- vapor-beta.rb
+++ vapor-beta.rb
@@ -1,8 +1,8 @@
 class VaporBeta < Formula
   homepage "https://vapor.codes"
-  version "3.0.0-beta.11"
-  url "https://github.com/vapor-cloud/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "0238e9b740b1a2821ae7896db7be464c96e057a7fea101f8f7b9fa6cdb0839d5"
+  version "3.0.0-beta.12"
+  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
+  sha256 "e8b17f5e64e5a6c1747ea09e3e48c9406b858bb157c8a122a26b824cfd776ec6"
 
   depends_on "ctls" => :run
 
