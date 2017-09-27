0ebf4b58e2a1dc2a86a041793c29dd96adbe4268
Tanner Nelson <me@tanner.xyz>
2017-09-27 15:52:21 -0400

    vapor-beta update

--- vapor-beta.rb
+++ vapor-beta.rb
@@ -1,14 +1,12 @@
 class VaporBeta < Formula
   homepage "https://vapor.codes"
-  version "3.0.0-beta.12"
+  version "3.0.0-beta.13"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "e8b17f5e64e5a6c1747ea09e3e48c9406b858bb157c8a122a26b824cfd776ec6"
+  sha256 "957006cdffff48c8d4f55277c998b967f52494ffc952c7feda1c0b80a60fe1c0"
 
   depends_on "ctls" => :run
 
   def install
-    lib.install "libCHTTP.dylib"
-    lib.install "libCSQLite.dylib"
     bin.install "vapor-beta"
   end
 end
