c58c528300f37cff44cb0713b5b5cc06a9631c9d
tanner0101 <me@tanner.xyz>
2017-10-05 23:25:03 -0400

    update toolbox to 3.0

--- vapor.rb
+++ vapor.rb
@@ -1,13 +1,12 @@
 class Vapor < Formula
   homepage "https://vapor.codes"
-  version "2.0.4"
-  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra"
-  sha256 "4bf020abeeaf2bfdaa57c2111ac31f35c05b22d4fafa721b5cb16a322c8c0404"
+  version "3.0.0"
+  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
+  sha256 "91a1055cf30d88c6447d9a6fa78b9ce70a38a7129929c0d294fad476f9f99d15"
 
   depends_on "ctls" => :run
 
   def install
-    system "mv macOS-sierra vapor"
     bin.install "vapor"
   end
 end
