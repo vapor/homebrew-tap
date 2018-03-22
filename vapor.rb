edf8f3149b23e1f0b8afa5c0f2690b391a05c1a0
tanner0101 <me@tanner.xyz>
2018-03-21 20:36:35 -0400

    fix version link

--- vapor.rb
+++ vapor.rb
@@ -1,7 +1,7 @@
 class Vapor < Formula
   homepage "https://vapor.codes"
   version "3.1.4.l"
-  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
+  url "https://github.com/vapor/toolbox/releases/download/3.1.4/macOS-sierra.tar.gz"
   sha256 "49f987fd12f493600e7989c63dda0b2c5f68c4b261ecc7042ccf1b2e75e53732"
 
   depends_on "ctls" => :run
