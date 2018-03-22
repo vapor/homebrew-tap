a2027fb33ce072b486c4e2c7bb47353b03e69f63
tanner0101 <me@tanner.xyz>
2018-03-21 20:35:28 -0400

    libressl dep

--- vapor.rb
+++ vapor.rb
@@ -1,10 +1,11 @@
 class Vapor < Formula
   homepage "https://vapor.codes"
-  version "3.1.4"
+  version "3.1.4.l"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "49f987fd12f493600e7989c63dda0b2c5f68c4b261ecc7042ccf1b2e75e53732"
 
   depends_on "ctls" => :run
+  depends_on "libressl" => :run
 
   def install
     bin.install "vapor"
