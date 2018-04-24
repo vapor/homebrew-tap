3049aaa3ac78002f13eb2f9575660931d8ed08fd
Tanner <me@tanner.xyz>
2018-04-24 09:37:00 -0400

    Update vapor.rb

--- vapor.rb
+++ vapor.rb
@@ -1,8 +1,8 @@
 class Vapor < Formula
   homepage "https://vapor.codes"
-  version "3.1.4.l"
-  url "https://github.com/vapor/toolbox/releases/download/3.1.4/macOS-sierra.tar.gz"
-  sha256 "49f987fd12f493600e7989c63dda0b2c5f68c4b261ecc7042ccf1b2e75e53732"
+ version "3.1.7"
+  url "https://github.com/vapor/toolbox/releases/download/3.1.7/macOS-sierra.tar.gz"
+ sha256 "d7790c42e05efd58e71861469d91d759ca31d96007c68765c555535ae59295e0"
 
   depends_on "ctls"
   depends_on "libressl"
