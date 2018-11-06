09d74512b767c70272d6c0b254271c00e752747c
Logan Wright <logan.william.wright@gmail.com>
2018-11-06 18:09:53 +0100

    Update vapor-beta.rb

--- vapor-beta.rb
+++ vapor-beta.rb
@@ -1,8 +1,8 @@
 class VaporBeta < Formula
   homepage "https://vapor.codes"
-  version "18.0.0-beta.7"
+  version "18.0.0-beta.8"
   url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "822566dd505c8ce111c0096967bb265b85786143adee0a7a843a4c7ccb885773"
+  sha256 "15ed145ba3916142df5294b571d113a545200a572a50da6cf3151f2833a2021c"
 
   def install
     bin.install "vapor-beta"
