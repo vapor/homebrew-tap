79dea389ca1d86306ecb7ad2cfa6dca70760730b
tanner0101 <me@tanner.xyz>
2019-12-09 18:58:12 -0500

    3.1.12

--- vapor.rb
+++ vapor.rb
@@ -2,12 +2,14 @@ class Vapor < Formula
   homepage "https://vapor.codes"
   version "3.1.12"
   url "https://github.com/vapor/toolbox/releases/download/3.1.12/macOS.zip"
-  sha256 "3a3d9ff9c2eb182ba4412b54c3987fca120b57f3b67c27f7cfac77a6a400065a"
+  sha256 "13dcd65f9afb998af39a0a5389958b3994bff18c9a063309303e265d0451a80e"
 
   depends_on "ctls"
   depends_on "libressl"
 
   def install
     bin.install "vapor"
+    lib.install "lib/libcrypto.1.0.0.dylib"
+    lib.install "lib/libssl.1.0.0.dylib"
   end
 end
