92545552085b7f922e95e53c8518d06474b14f16
rpstro02 <email>
2020-04-07 23:36:21 -0400

    Update vapor-beta.rb
    Revved vapor beta toolbox version

--- vapor-beta.rb
+++ vapor-beta.rb
@@ -6,7 +6,7 @@ class VaporBeta < Formula
   depends_on :xcode => "11"
 
   stable do
-    version "18.0.0-beta.27"
+    version "18.0.0-beta.28"
     url "https://github.com/vapor/toolbox/archive/#{version}.tar.gz"
     sha256 "3f031f885fc4cfac9cabb6cb86124ad1283cd75ce58bc5804774c04af45e6d03"
   end
